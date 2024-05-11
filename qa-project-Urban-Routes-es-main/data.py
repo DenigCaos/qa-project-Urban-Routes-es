ba7c660b2e6094371296c544c2908958b106ecec
DenigCaos <167912841+DenigCaos@users.noreply.github.com>
2024-05-10 21:50:24 -0500

    Delete qa-project-Urban-Routes-es-main directory

--- qa-project-Urban-Routes-es-main/data.py
+++
@@ -1,6 +0,0 @@
-urban_routes_url = 'https://cnt-f107a5b8-fbe6-4637-aaf2-8ee86579f3d2.containerhub.tripleten-services.com?lng=es'
-address_from = 'East 2nd Street, 601'
-address_to = '1300 1st St'
-phone_number = '+1 123 123 12 12'
-card_number, card_code = '1234 5678 9100', '111'
-message_for_driver = 'Muéstrame el camino al museo'
